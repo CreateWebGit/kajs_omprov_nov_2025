ae0ae67e5b1d54e9628d3fecd6204260ce7f1e33
Kaj <hej@createweb.se>
2025-11-06 11:27:43 +0100

    Update question2.js with the answare

--- question2.js
+++ question2.js
@@ -11,3 +11,32 @@
 // och metoden
 // addBook som lägger till en bok till författarens lista
 // listBooks som visar en lista på författarens böcker
+
+// ES6-klass: Book
+export class Book {
+  constructor(title, author, rating) {
+    this.title = title;
+    this.author = author;
+    this.rating = rating;
+  }
+
+  displayBook() {
+    return `${this.title} – ${this.author} (${this.rating}/5)`;
+  }
+}
+
+// ES6-klass: Author
+export class Author {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    this.books.push(book);
+  }
+
+  listBooks() {
+    return this.books.map((b) => b.displayBook());
+  }
+}
